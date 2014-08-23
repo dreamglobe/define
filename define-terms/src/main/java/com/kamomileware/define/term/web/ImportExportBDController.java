b3537ba9cc279d89c3e02b7a7d342a68766b9bb5
dreamglobe <iigarsan@gmail.com>
2014-08-23 19:04:24 +0000

    close #139 Punto de acceso para iniciar la inicialización de BD close Punto de acceso para desencadenar el volcado de BD
    git-svn-id: http://karma-server/svn/define/branches/term-services@142 1bd3b847-742a-40ef-b5c9-d6ca6d23043b

--- define-terms/src/main/java/com/kamomileware/define/term/web/ImportExportBDController.java
+++ define-terms/src/main/java/com/kamomileware/define/term/web/ImportExportBDController.java
@@ -1,6 +1,6 @@
 package com.kamomileware.define.term.web;
 
-import com.kamomileware.define.term.dbproc.DBService;
+import com.kamomileware.define.term.service.DBService;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,7 +23,7 @@ import java.nio.charset.StandardCharsets;
  * Created by pepe on 21/08/14.
  */
 @Controller
-@RequestMapping("/bd")
+@RequestMapping("/db")
 public class ImportExportBDController {
 
     private static final Logger logger = LoggerFactory.getLogger(ImportExportBDController.class);
@@ -34,7 +33,7 @@ public class ImportExportBDController {
 
     @RequestMapping(value = "/init", method = RequestMethod.POST)
     public ResponseEntity<String> createBD(@RequestParam("file") MultipartFile file) {
-        if (!file.isEmpty() && "application/json".equals(file.getContentType())) {
+        if (!file.isEmpty() ) { //&& "application/json".equals(file.getContentType()
             try {
                 byte[] bytes = file.getBytes();
                 String serializedDB = new String(bytes);
@@ -42,7 +41,7 @@ public class ImportExportBDController {
                 dbService.importBD(serializedDB);
                 return new ResponseEntity<>(Long.toString(file.getSize()), null, HttpStatus.CREATED);
             } catch (Exception e) {
-                logger.error(String.format("Error importing BD from uploaded {0}.", file.getName()), e);
+                logger.error(String.format("Error importing BD from uploaded %s.", file.getName()), e);
                 return new ResponseEntity<>(e.getMessage(), null, HttpStatus.INTERNAL_SERVER_ERROR);
             }
         } else {
@@ -50,15 +49,20 @@ public class ImportExportBDController {
         }
     }
 
+    @RequestMapping(value = "/export", method = RequestMethod.GET)
+    public void exportDB(HttpServletResponse response){
+        this.exportDB("term_db", response);
+    }
+
     @RequestMapping(value = "/export/{fileName}", method = RequestMethod.GET)
     public void exportDB(@PathVariable("fileName") String fileName,
                          HttpServletResponse response) {
-        String finalFileName = StringUtils.isEmpty(fileName) ? "term_db.json" : fileName;
+        String finalFileName = fileName + ".json";
         try {
             final byte[] bytes = dbService.export().getBytes(StandardCharsets.UTF_8);
             writeExportOnResponse(finalFileName, response, bytes);
         } catch (IOException ex) {
-            logger.error(String.format("Error writing file to output stream to {0}", finalFileName));
+            logger.error(String.format("Error writing file to output stream to %s", finalFileName));
             response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
         }
     }
@@ -68,7 +72,7 @@ public class ImportExportBDController {
         response.setContentType("application/json");
         response.setContentLength(bytes.length);
         response.setCharacterEncoding("UTF-8");
-        response.addHeader("Content-Disposition", String.format("attachment; filename=\"{0}\"", fileName));
+        response.addHeader("Content-Disposition", String.format("attachment; filename=\"%s\"", fileName));
         response.flushBuffer();
     }
 
